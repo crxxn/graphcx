2e35248ee22e8b7fbe40563ffe39a11ad6c03a1d
Oliver Wiedemann <mail@oliver-wiedemann.net>
2016-10-11 10:06:07 +0100

    working final submission

--- is16051149.java
+++ is16051149.java
@@ -6,12 +6,33 @@ import java.io.PrintWriter;
 import java.io.BufferedReader;
 import java.util.ArrayList;
 import java.util.Random;
+
+import javax.swing.JFrame;
+
 import java.lang.Exception;
 import java.lang.Math;
 
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLCapabilities;
+import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.GLProfile;
+import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.GL;
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.glu.GLU;
+
+import java.awt.Dimension;
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+
+
 class is16051149 {
 	public static void main(String[] args) {
-
+	
+		
+		
 		File input = new File("input.txt");
 		/* parse an incidence matrix from the given input file */
 		IncidenceMatrix inc= IncidenceMatrix.parseAsciiMatrix(input);
@@ -41,10 +62,11 @@ class is16051149 {
 			System.err.println("Error writing to output file");
 			System.exit(1);
 		}
+		
+		rep.simulated_annealing(rep.ord.n * 2, 0.001, 0, true);
 	
 		
-		
-		
+		rep.draw(); //actually not necessary if we call simulated_annealing with draw_progress=true
 		
 	}
 }
@@ -55,26 +77,91 @@ class GraphRepresentation {
 	/* just keep them public to be able to manipulate them more directly */
 	public AdjacencyMatrix mat;
 	public Ordering ord;
+	private double chunk;
+
+	public void simulated_annealing(double temperature, double cr, double min_temp, boolean draw_progress) {
+		ord.randomize(); /* randomize current ordering */
+		if(draw_progress) {
+			draw();
+		}
+		double fit = fitness();
+		
+		while (temperature > min_temp) {
+			Ordering tmp_ord = new Ordering(ord);
+			double tmp_fit = fit;
+			
+			for (int i = 0; i < temperature; i++) {
+				tmp_ord.mutate();
+			}
+			tmp_fit = fitness(tmp_ord);
+			
+			if(tmp_fit < fit) {
+				fit = tmp_fit;
+				this.ord = new Ordering(tmp_ord);
+				if(draw_progress) {
+					draw();
+				}
+			}
+			temperature = temperature-cr;
+		}
+		
+	}
+	
 	
 	public GraphRepresentation(AdjacencyMatrix mat) {
 		this.mat = mat;
 		this.ord = new Ordering(mat.getColumns());
+		chunk = 2 * Math.PI / ord.n;
+	}
+
+	
+	public double fitness() {
+		return fitness(this.ord);
+	}
+
+	/* return fitness value for the current representation */
+	public double fitness(Ordering ord) {
+		double[] x = new double[ord.n];
+		double[] y = new double[ord.n];
+		
+		/* calculate cartesian coordinates for each node of
+		 * the current representation */
+		for (int k=0; k<ord.n; k++) {
+			x[k] = Math.cos(ord.data[k] * chunk);
+			y[k] = Math.sin(ord.data[k] * chunk);
+		}
+		
+		/* sum up the distance between adjacent nodes */
+		double result = 0;
+		/* iterate over the upper triangular matrix */
+		for (int k=0; k<ord.n; k++) {
+			for (int l=k+1; l<ord.n; l++) {
+				if (mat.get(k, l) == 1) {
+					/* add the distance between to adjacent nodes to the current fitness result */
+					result = result + Math.sqrt( Math.pow(x[k]-x[l],2) + Math.pow(y[k]-y[l],2) );
+				}
+			}
+		}
+		return result;
 	}
 	
 	public void draw() {
-		
+		GraphDrawer gd = GraphDrawer.getInstance();
+		gd.updateData(this);
 	}
 	
 	
 }
 
 class Ordering {
-	
+	private static Random rg = new Random(System.currentTimeMillis());
 	int[] data;
 	int n; /* size of the data array */
 
+
 	/* initialise an ascending n-ordering */
 	public Ordering(int n) {
+
 		this.n = n;
 		data = new int[n];
 		for (int k=0; k<n; k++) {
@@ -82,10 +169,16 @@ class Ordering {
 		}
 	}
 
+	/* copy constructor */
+	public Ordering(Ordering o) {
+
+		this.data = new int[o.n];
+		/* the next line was missing and caused a serious bug */
+		System.arraycopy(o.data, 0, this.data, 0, o.n);
+		this.n = o.n;
+	}
+
 	public void randomize() {
-		/* seed rng with current time */
-		Random rg = new Random(System.currentTimeMillis());
-		
 		/* this algo is known as fisher-yates shuffle */ 
 		for (int k=n-1; k>0; k--) {
 			int l = rg.nextInt(k+1); /* generate a random integer in [0,k] */
@@ -93,6 +186,14 @@ class Ordering {
 			data[k] = data[l];
 			data[l] = tmp;
 		}
+	}
+	
+	public void mutate() {
+		int a = rg.nextInt(n);
+		int b = rg.nextInt(n);
+		int tmp = data[a];
+		data[a] = data[b];
+		data[b] = tmp;
 	}
 	
 	public String toString() {
@@ -294,3 +395,125 @@ class IncidenceMatrix extends GraphMatrix {
 	}
 }
 
+class GraphDrawer  {
+
+	private static GraphDrawer gd = null;
+	final GLProfile glprofile;
+	final GLCapabilities glcapabilities;
+	final GLCanvas glcanvas;
+	GLEventListener gleventlistener;
+	final JFrame frame;
+
+	public static GraphDrawer getInstance() {
+		if (GraphDrawer.gd == null) {
+			return new GraphDrawer();
+		} else {
+			return gd;
+		}
+		
+	}
+	
+	private GraphDrawer() {
+		glprofile = GLProfile.get(GLProfile.GL2);
+		glcapabilities = new GLCapabilities( glprofile );
+		glcanvas = new GLCanvas( glcapabilities );
+		gleventlistener = new VisEventListener();
+		glcanvas.addGLEventListener(gleventlistener);
+		frame = new JFrame("Graph Visualization");
+		frame.getContentPane().add(glcanvas);
+		frame.setSize(frame.getContentPane().getPreferredSize());
+		//frame.setPreferredSize(new Dimension(800, 600));
+		frame.setResizable(false);
+		frame.setMinimumSize(new Dimension(1024, 768));
+
+		frame.setVisible(true);
+		//close on exit window event 
+		frame.addWindowListener( new WindowAdapter() {
+			public void windowClosing( WindowEvent windowevent ) {
+				frame.remove( glcanvas );
+				frame.dispose();
+				System.exit(0);
+			}
+		}); 
+		GraphDrawer.gd = this;
+		
+	}
+	
+	public void updateData(GraphRepresentation rep) {
+		GraphViz.setRenderData(rep);
+		glcanvas.display();
+	}
+	
+	
+}
+
+/* EventListener for the visualization */
+class VisEventListener implements GLEventListener {
+	public void display(GLAutoDrawable glautodrawable) {
+		GraphViz.render(glautodrawable.getGL().getGL2(), glautodrawable.getSurfaceWidth(), glautodrawable.getSurfaceHeight());
+	}
+	public void dispose(GLAutoDrawable arg0) {}
+	public void init(GLAutoDrawable arg0) {}
+	public void reshape(GLAutoDrawable glautodrawable, int x, int y, int width, int height) {}
+}
+
+class GraphViz {
+	
+	private static GraphRepresentation rep;
+
+	protected static void setRenderData(GraphRepresentation r) {
+		rep = r;
+	}
+
+	
+	protected static void render( GL2 gl2, int width, int height ) {
+
+		double radius = 0.8;
+		double scale = 0.01;
+		
+		gl2.glClear(GL.GL_COLOR_BUFFER_BIT);
+	
+		
+		for (int i=0; i<rep.ord.n; i++) {
+			for (int j=i+1; j<rep.ord.n; j++) {
+				if (rep.mat.get(i, j) == 1) {
+					float x1 = (float) (radius * Math.cos(2*Math.PI*rep.ord.data[i]/rep.ord.n));
+					float y1 = (float) (radius * Math.sin(2*Math.PI*rep.ord.data[i]/rep.ord.n));
+					float x2 = (float) (radius * Math.cos(2*Math.PI*rep.ord.data[j]/rep.ord.n));
+					float y2 = (float) (radius * Math.sin(2*Math.PI*rep.ord.data[j]/rep.ord.n));
+					drawEdge(gl2, x1, y1, x2, y2);
+				}
+			}
+		}
+
+		for(int i=0; i<rep.ord.n; i++) {
+			gl2.glLoadIdentity();
+			double x = radius * Math.cos(2*Math.PI*rep.ord.data[i]/rep.ord.n);
+			double y = radius * Math.sin(2*Math.PI*rep.ord.data[i]/rep.ord.n);
+			drawNode(gl2, x, y, scale);
+		}
+	}
+
+	/* draw an edge between the coordinates (x1,y1) and (x2,y2) */
+	private static void drawEdge(GL2 gl2, float x1, float y1, float x2, float y2 ) {
+		gl2.glBegin( GL2.GL_LINE_STRIP );
+		gl2.glColor3f(0.75f, 0.75f, 0.75f);
+		gl2.glVertex3f(x1, y1, 0f);
+		gl2.glVertex3f(x2, y2, 0f);
+		gl2.glEnd();
+		
+	}
+	/* draw a node at the coordinate (x,y) */
+	private static void drawNode(GL2 gl2, double x, double y, double scale) {
+		gl2.glBegin( GL2.GL_POLYGON );
+		gl2.glColor3f(0.19f,0.9f,0.79f);
+	
+		int circle_resolution = 30;
+		for(int i=0; i<circle_resolution; i++) {
+			float norm_x = (float)(scale*Math.cos(2*Math.PI*i/circle_resolution) + x);
+			float norm_y = (float)(scale*Math.sin(2*Math.PI*i/circle_resolution) + y);
+			gl2.glVertex3f( norm_x, norm_y, 0f );
+		}
+		gl2.glEnd();
+	}
+}
